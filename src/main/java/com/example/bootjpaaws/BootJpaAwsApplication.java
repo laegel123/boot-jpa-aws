ec6700b682ae66a02061ac950b0a2175b45b94a7
laegel <laegel1@gmail.com>
2022-03-30 22:24:56 +0900

    1차 커밋

---
+++ src/main/java/com/example/bootjpaaws/BootJpaAwsApplication.java
@@ -0,0 +1,15 @@
+package com.example.bootjpaaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BootJpaAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJpaAwsApplication.class, args);
+    }
+
+}
